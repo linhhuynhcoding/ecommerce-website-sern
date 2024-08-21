f85ae79addf2aef11c3a6f571eb04cc4c3e5ecd0
linhhuynhcoding <nhatlinh.6449@gmail.com>
2024-08-21 13:17:29 +0700

    static file & scss

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3004;
 
+//STATIC FILE
+app.use(express.static(path.join(__dirname, 'public')));
+
 //HTTP LOGGER 
 app.use(morgan('combined'));
 
 //TEMPLATE ENGINE
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
 });
 
 app.listen(port, () => console.log(`Example app lisening at http://localhost:${port}`));
