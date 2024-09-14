2e6a51d7ae60440bb75565ecd718de527de27d39
linhhuynhcoding <nhatlinh.6449@gmail.com>
2024-09-14 13:59:42 +0700

    fix vercel

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-require('dotenv').config({ path: '../backend/.env' })
+// require('dotenv').config({ path: '../backend/.env' })
 
 const instance = axios.create({
-    baseURL: `${process.env.URL_BACKEND}:{process.env.PORT_BACKEND}`,
+    // baseURL: `${process.env.URL_BACKEND}:{process.env.PORT_BACKEND}`,
     timeout: 3 * 60 * 1000,
     headers: {'X-Custom-Header': 'foobar'},
     withCredentials: true,
